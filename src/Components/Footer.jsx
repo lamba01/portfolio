0add5e3d80de625cf1078d89c79b0a936ca9145b
lamba01 <jamesspaul987@gmail.com>
2023-09-18 09:51:50 +0100

    adjusted footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,25 +1,11 @@
   
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import "../styles/footer.css";
-import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
-import { SiFrontendmentor } from 'react-icons/si';
 
-function Footer() {
-  const [activeName, setActiveName] = useState('Twitter'); // Set the default name here
-
-  const handleMouseEnter = (name) => {
-    setActiveName(name);
-  };
-
-  useEffect(() => {
-    // Set the default name when the component mounts
-    setActiveName('Twitter'); // Change this to the desired default name
-  }, []);
-
+function Footer(){
   return (
     <footer>
       <p>&copy; 2023. Developed by John. All rights reserved</p>
-     
       </footer>
   );
 }
